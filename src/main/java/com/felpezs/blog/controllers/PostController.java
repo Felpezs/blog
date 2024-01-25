1fe242b38bcfe69964a9182a6b5c7e462102cbb1
felipe <felipe@knowcode.app>
2024-01-25 20:58:04 -0300

    feat: using the post service

--- src/main/java/com/felpezs/blog/controllers/PostController.java
+++ src/main/java/com/felpezs/blog/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.felpezs.blog.controllers;
 import com.felpezs.blog.dtos.PostRecordDto;
 import com.felpezs.blog.models.PostModel;
 import com.felpezs.blog.repositories.PostRepository;
+import com.felpezs.blog.services.PostService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +17,18 @@ import java.io.IOException;
 @RequestMapping("/api/posts")
 public class PostController {
 
-    @Autowired
-    PostRepository postRepository;
+    public PostService getPostService() {
+        return postService;
+    }
+
+    final PostService postService;
+    public PostController(PostService postService){
+        this.postService = postService;
+    }
 
     @PostMapping(consumes = {"multipart/form-data"})
     public ResponseEntity<Object> saveProduct(@Valid @ModelAttribute PostRecordDto postRecordDto){
-        var postModel = new PostModel();
-
-        try{
-            postModel.setTitle(postRecordDto.title());
-            postModel.setSubtitle(postRecordDto.subtitle());
-            postModel.setData(postRecordDto.data().getBytes());
-
-            return ResponseEntity.status(HttpStatus.CREATED).body(postRepository.save(postModel));
-        }catch (IOException exception){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error when writing the file.");
-        }
+        var postModel = this.getPostService().insert(postRecordDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(postModel);
     }
 }
