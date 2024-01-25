9271289d9afec7a81e99bde099c2370b3fd8ecae
felipe <felipe@knowcode.app>
2024-01-25 20:57:36 -0300

    feat: postrecordDto to model

--- src/main/java/com/felpezs/blog/models/PostModel.java
+++ src/main/java/com/felpezs/blog/models/PostModel.java
@@ -1,16 +1,20 @@
 package com.felpezs.blog.models;
 
+import com.felpezs.blog.dtos.PostRecordDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.IOException;
 import java.time.Instant;
 
 @Document("Posts")
 @Getter
 @Setter
+@NoArgsConstructor
 public class PostModel {
     @Id
     private String id;
@@ -19,4 +23,11 @@ public class PostModel {
     @CreatedDate
     private Instant createdAt;
     private byte[] data;
+
+    public PostModel(PostRecordDto postRecordDto) throws IOException {
+
+        this.title = postRecordDto.title();
+        this.subtitle = postRecordDto.subtitle();
+        this.data = postRecordDto.data().getBytes();
+    }
 }
